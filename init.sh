6913e9ba81dcd4930eab70be53ee38c38882cb5d
vonziper <vonziper@gmail.com>
2017-10-18 03:46:00 -0400

    added init script

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# TODO : make that generic
+cp .gitconfig ~/.gitconfig
+cp .gitignore_global ~/.gitignore_global
+cp .zshrc ~/.zshrc
+cp com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
